1ad9788766bc19fd6031ee3dadf55d4037fc6619
boreddad <ronnebaumfinances@gmail.com>
2023-08-19 10:56:45 -0400

    feat: fully bootstrap project

--- bin/main.ml
+++ bin/main.ml
@@ -26,30 +26,83 @@ let get_project_type () =
   | _ -> None
 ;;
 
-let create_main_cmake name binary_type =
-  match binary_type with
-  | Binary -> Cmake.bin_main_cmake_file name |> Printf.printf "%s\n"
-  | Library -> Cmake.lib_main_cmake_file name |> Printf.printf "%s\n"
+let create_main_cmake name binary_type path =
+  let channel = open_out path in
+  (match binary_type with
+   | Binary -> Cmake.bin_main_cmake_file name |> output_string channel
+   | Library -> Cmake.lib_main_cmake_file name |> output_string channel);
+  close_out channel
 ;;
 
-let create_main_file name binary_type =
-  match binary_type with
-  | Binary -> Src.main_bin_file () |> Printf.printf "%s\n"
-  | Library ->
-    Src.main_lib_file () |> Printf.printf "%s\n";
-    Src.main_lib_header name |> Printf.printf "%s\n"
+let create_main_file name binary_type file_path hdr_path =
+  let file_channel = open_out file_path in
+  (match binary_type with
+   | Binary -> Src.main_bin_file () |> output_string file_channel
+   | Library ->
+     let hdr_channel = open_out hdr_path in
+     Src.main_lib_file () |> output_string file_channel;
+     Src.main_lib_header name |> output_string hdr_channel;
+     close_out hdr_channel);
+  close_out file_channel
 ;;
 
-let create_test_cmake name binary_type =
-  match binary_type with
-  | Binary -> Cmake.test_bin_cmake_file name |> Printf.printf "%s\n"
-  | Library -> Cmake.test_lib_cmake_file name |> Printf.printf "%s\n"
+let create_test_cmake name binary_type path =
+  let channel = open_out path in
+  (match binary_type with
+   | Binary -> Cmake.test_bin_cmake_file name |> output_string channel
+   | Library -> Cmake.test_lib_cmake_file name |> output_string channel);
+  close_out channel
 ;;
 
-let create_test_file name binary_type =
+let create_test_file name binary_type path =
+  let channel = open_out path in
+  (match binary_type with
+   | Binary -> Src.bin_test_file name |> output_string channel
+   | Library -> Src.lib_test_file name |> output_string channel);
+  close_out channel
+;;
+
+let create_dir name =
+  try
+    let src_dir_fmt = format_of_string "%s/src" in
+    let tests_dir_fmt = format_of_string "%s/tests" in
+    let src_dir = Printf.sprintf src_dir_fmt name in
+    let test_dir = Printf.sprintf tests_dir_fmt name in
+    if not (Sys.file_exists name) then Sys.mkdir name 0o755;
+    if not (Sys.file_exists src_dir) then Sys.mkdir src_dir 0o755;
+    if not (Sys.file_exists test_dir) then Sys.mkdir test_dir 0o755
+  with
+  | Sys_error _ ->
+    Printf.printf "failed to create directory %s\n" name;
+    exit 1
+;;
+
+let create_gitignore path =
+  let channel = open_out path in
+  output_string channel "build";
+  close_out channel;
+;;
+
+let init name binary_type =
+  let main_cmake_path_fmt = format_of_string "%s/CMakeLists.txt" in
+  let gitignore_path_fmt = format_of_string "%s/.gitignore" in
+  let test_cmake_path_fmt = format_of_string "%s/tests/CMakeLists.txt" in
+  let src_path_fmt = format_of_string "%s/src/%s.c" in
+  let hdr_path_fmt = format_of_string "%s/src/%s.h" in
+  let test_path_fmt = format_of_string "%s/tests/%s_test.c" in
+  create_dir name;
+  create_gitignore (Printf.sprintf gitignore_path_fmt name);
+  Printf.sprintf main_cmake_path_fmt name |> create_main_cmake name binary_type;
+  Printf.sprintf test_cmake_path_fmt name |> create_test_cmake name binary_type;
+  create_main_file
+    name
+    binary_type
+    (Printf.sprintf src_path_fmt name name)
+    (Printf.sprintf hdr_path_fmt name name);
+  Printf.sprintf test_path_fmt name name |> create_test_file name binary_type;
   match binary_type with
-  | Binary -> Src.bin_test_file name |> Printf.printf "%s\n"
-  | Library -> Src.lib_test_file name |> Printf.printf "%s\n"
+  | Binary -> Printf.printf "initialized binary project in %s\n" name
+  | Library -> Printf.printf "initialized library project in %s\n" name
 ;;
 
 let () =
@@ -60,15 +113,12 @@ let () =
       Printf.printf "Invalid name\n";
       exit 1
   in
-  let project_type =
+  let binary_type =
     match get_project_type () with
     | Some pt -> pt
     | None ->
       Printf.printf "no type\n";
       exit 1
   in
-  create_main_cmake name project_type;
-  create_main_file name project_type;
-  create_test_cmake name project_type;
-  create_test_file name project_type
+  init name binary_type
 ;;
